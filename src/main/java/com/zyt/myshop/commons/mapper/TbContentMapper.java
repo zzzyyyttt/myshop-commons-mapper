3fab5adde616ac386579aaa0759f8e9a58c7cdbc
wszyt <173982112@qq.com>
2019-06-19 16:48:07 +0800

    commit myshop 后台管理

--- src/main/java/com/zyt/myshop/commons/mapper/TbContentMapper.java
+++ src/main/java/com/zyt/myshop/commons/mapper/TbContentMapper.java
@@ -1,7 +1,10 @@
 package com.zyt.myshop.commons.mapper;
 
 import com.zyt.myshop.commons.domain.TbContent;
+import com.zyt.myshop.commons.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 import tk.mybatis.mapper.MyMapper;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbContentMapper extends MyMapper<TbContent> {
 }
